c0e7708b3b04fde2b5e38c88ba1fc13273a4cdf4
Renzo Otoya//..quit <renzo.aldair.otoya@gmail.com>
2025-12-21 02:11:38 -0500

    commit

--- frontend/app/economy/calendar/page.tsx
+++
@@ -1,9 +0,0 @@
-import EconomicCalendarUI from "@/components/EconomicCalendarUI";
-
-export default function CalendarPage() {
-  return (
-    <div className="p-8">
-      <EconomicCalendarUI />
-    </div>
-  );
-}
